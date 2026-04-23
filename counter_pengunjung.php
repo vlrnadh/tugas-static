31d1cb1e9541623f6abc742c1f40289e6d3302ac
vlrnadh <vathratiagrenadha@gmail.com>
2026-04-22 17:35:01 -0700

    Praktikum 1 After: sudah ada method reset

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -5,6 +5,10 @@ class Pengunjung {
     public function __construct() {
         self::$jumlah++;
     }
+
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
@@ -13,5 +17,7 @@ $p3 = new Pengunjung();
 $p4 = new Pengunjung();
 $p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+echo "Jumlah Pengunjung (before reset): " . Pengunjung::$jumlah . "<br>";
+Pengunjung::reset();
+echo "Jumlah Pengunjung (after reset): " . Pengunjung::$jumlah . "<br>";
 ?>
