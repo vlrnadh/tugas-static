d22dda77671f012833b8cb1bd3b04d2c7cb03677
vlrnadh <vathratiagrenadha@gmail.com>
2026-04-22 17:37:09 -0700

    Praktikum 2 Before: hanya ada method kali dan bagi

--- praktikum2.php
+++ praktikum2.php
@@ -7,23 +7,9 @@ class Matematika {
     public static function bagi($a, $b) {
         return $a / $b;
     }
-
-    public static function tambah($a, $b) {
-        return $a + $b;
-    }
-
-    public static function kurang($a, $b) {
-        return $a - $b;
-    }
-
-    public static function luasPersegiPanjang($sisi) {
-        return $sisi * $sisi;
-    }
 }
 
-echo "Kali: " . Matematika::kali(4, 5) . "<br>";
-echo "Bagi: " . Matematika::bagi(10, 2) . "<br>";
-echo "Tambah: " . Matematika::tambah(3, 7) . "<br>";
-echo "Kurang: " . Matematika::kurang(10, 4) . "<br>";
-echo "Luas Persegi: " . Matematika::luasPersegiPanjang(5) . "<br>";
+echo Matematika::kali(4, 5);
+echo "<br>";
+echo Matematika::bagi(10, 2);
 ?>
